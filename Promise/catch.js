783de452e799d5dfc8a5701174e47f6cfb867b64
Duduman Bogdan <bogdan.duduman@cegeka.com>
2018-05-09 17:55:55 +0300

    add fixes to catch promise solution

--- Promise/catch.js
+++ Promise/catch.js
@@ -1,77 +1,85 @@
 // 79: Promise - catch
 // To do: make all tests pass, leave the assert lines unchanged!
-// Here we use promises to trigger, don't modify the block with the
+// Here we use promises to trigger, don't modify the block with the 
 // returning promise!
 
-describe("`catch()` returns a Promise and deals with rejected cases only", () => {
-  describe("prerequisites for understanding", () => {
-    it("*return* a fulfilled promise, to pass a test", () => {
-      return Promise.resolve((resolve, reject) => {});
+describe('`catch()` returns a Promise and deals with rejected cases only', () => {
+
+  describe('prerequisites for understanding', () => {
+
+    it('*return* a fulfilled promise, to pass a test', () => {
+      return Promise.resolve();
       assert(false); // Don't touch! Make the test pass in the line above!
     });
 
-    it("reminder: the test passes when a fulfilled promise is returned", () => {
-      return Promise.resolve("I should fulfill.");
+    it('reminder: the test passes when a fulfilled promise is returned', () => {
+      return Promise.resolve('I should fulfill.');
     });
+
   });
 
-  describe("`catch` method basics", () => {
-    it("is an instance method", () => {
-      const p = new Promise(() => {});
-      assert.equal(typeof p.catch, "function");
+  describe('`catch` method basics', () => {
+    it('is an instance method', () => {
+      const p = Promise.reject();
+      assert.equal(typeof p.catch, 'function');
     });
 
-    it("catches only promise rejections", done => {
+    it('catches only promise rejections', (done) => {
       const promise = Promise.reject();
       promise
-        .then(() => {
-          done("Should not be called!");
-        })
+        .then(() => { done('Should not be called!'); })
         .catch(done);
     });
 
-    it("returns a new promise", () => {
+    it('returns a new promise', () => {
       const whatToReturn = () => Promise.reject();
-      const promise = Promise.reject();
-      return promise.catch(() => whatToReturn);
+      const promise = Promise.resolve(whatToReturn);
+      return promise.catch(() =>
+        whatToReturn()
+      );
     });
 
-    it("converts it`s return value into a promise", () => {
+    it('converts it`s return value into a promise', () => {
       const p = Promise.reject();
-      const p1 = p.catch(() => "promise?");
+      const p1 = p.catch(() => 'promise?');
 
-      return p1.then(result => assert.equal("promise?", result));
+      return p1.then(result => assert.equal('promise?', result));
     });
 
-    it("the first parameter is the rejection reason", () => {
-      const p = Promise.reject("oops");
+    it('the first parameter is the rejection reason', () => {
+      const p = Promise.reject('oops');
 
       return p.catch(reason => {
-        assert.equal(reason, "oops");
+        assert.equal(reason, 'oops');
       });
     });
   });
 
-  describe("multiple `catch`es", () => {
-    it("only the first `catch` is called", () => {
-      const p = Promise.reject("1");
+  describe('multiple `catch`es', () => {
+    it('only the first `catch` is called', () => {
+      const p = Promise.reject('1');
       const p1 = p
-        .catch(reason => reason + " AND 2")
-        .catch(reason => `${reason} AND 2`);
+          .catch(reason =>'1 AND 2')
+          .catch(reason => `${reason} AND 3`)
+        ;
 
-      return p1.then(result => assert.equal(result, "1 AND 2"));
+      return p1.then(result =>
+        assert.equal(result, '1 AND 2')
+      );
     });
 
-    it("if a `catch` throws, the next `catch` catches it", () => {
-      const p = Promise.reject("1");
+    it('if a `catch` throws, the next `catch` catches it', () => {
+      const p = Promise.reject('1');
       const p1 = p
-        .catch(reason => {
-          throw Error(`${reason} AND 2`);
-        })
-        .catch(err => `${err.message} AND 3`)
-        .catch(err => `${err} but NOT THIS`);
+          .catch(reason => { throw Error(`${reason} AND 2`) })
+          .catch(err => { throw Error(`${err.message} AND 3`) })
+          .catch(err => err.message)
+        ;
 
-      return p1.then(result => assert.equal(result, "1 AND 2 AND 3"));
+      return p1.then(result =>
+        assert.equal(result, '1 AND 2 AND 3')
+      );
     });
   });
+
 });
